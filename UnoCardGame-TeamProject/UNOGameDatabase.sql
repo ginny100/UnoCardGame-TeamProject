f973a61f83b9ae22ca5eacf081342c9a9379ff50
Diante Woodson <47933046+woodsond@users.noreply.github.com>
2021-04-26 15:30:21 -0500

    Database files along with a test file
    Here is the database .sql files. Also included is a test file that can be used to test user auth, player wins, etc.

---
+++ UnoCardGame-TeamProject/UNOGameDatabase.sql
@@ -0,0 +1,31 @@
+-- UNO Game DATABASE
+-- Drop all tables to prevent problems when creating them at the beginning
+DROP TABLE wins;
+DROP TABLE users;
+
+--Create all tables to be used
+CREATE TABLE users
+(
+	username VARCHAR(25),
+	password VARCHAR(16)
+);
+
+CREATE TABLE wins
+(
+	username VARCHAR(25),
+	wins INTEGER(3)
+);
+
+--Create the table constraints 
+ALTER TABLE users
+	ADD CONSTRAINT pk_users
+	PRIMARY KEY(username);
+	
+ALTER TABLE wins
+	ADD CONSTRAINT pk_wins
+	PRIMARY KEY(username);
+
+--Create the foreign key table constraints
+ALTER TABLE wins
+	ADD CONSTRAINT fk_wins
+	FOREIGN KEY(username) REFERENCES users(username);
---
+++ UnoCardGame-TeamProject/testUNOGameDatabase.sql
@@ -0,0 +1,10 @@
+--Testing UNOGAME DATABASE
+
+--Insert into the database
+INSERT INTO users (username, password)
+VALUES
+	{'g', aes_encrypt('e','key')),
+	('d', aes_encrypt('w', 'key')),
+	('g', aes_encrypt('d', 'key')),
+	('c', aes_encrypt('h', 'key'));
+
